b8d3173c31655110fd7abf28a64c38087d805da2
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-06-15 17:20:14 +0900

    Save sha value when send pull request

--- create_pull_request.rb
+++ create_pull_request.rb
@@ -1,5 +1,6 @@
 require "erb"
 require "octokit"
+require "sequel"
 
 head_branch_name = ARGV.first
 base_branch_name = ENV["BASE_BRANCH_NAME"]
@@ -16,6 +17,11 @@ client = Octokit::Client.new(access_token: ENV["GITHUB_AUTH_TOKEN"])
 pr = client.create_pull_request(repo, base_branch_name, head_branch_name, head_branch_name)
 pr_id = pr["number"].to_i
 pr_files = client.pull_request_files(repo, pr_id)
+
+db = Sequel.connect(ENV["DATABASE_URL"])
+
+table = db[:pull_request_history]
+table.insert(sha: pr["head"]["sha"])
 
 # 変更を監視するファイルパスのリスト
 WATCH_LIST = [
